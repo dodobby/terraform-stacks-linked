c026782497c54373d80d2d7994ac64e53e4d8e3c
admin <hj.do@cloudnetworks.co.kr>
2025-10-23 17:45:47 +0900

    Edit ephemeral

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,45 +1,54 @@
 variable "name_prefix" {
   description = "Prefix for resource names"
   type        = string
+  ephemeral   = true
 }
 
 variable "environment" {
   description = "Environment name (dev, stg, prd)"
   type        = string
+  ephemeral   = true
 }
 
 variable "vpc_id" {
   description = "VPC ID"
   type        = string
+  ephemeral   = true
 }
 
 variable "public_subnet_ids" {
   description = "Public subnet IDs for ALB"
   type        = list(string)
+  ephemeral   = true
 }
 
 variable "private_subnet_ids" {
   description = "Private subnet IDs for EC2 instances"
   type        = list(string)
+  ephemeral   = true
 }
 
 variable "web_security_group_id" {
   description = "Web security group ID"
   type        = string
+  ephemeral   = true
 }
 
 variable "ec2_instance_profile_arn" {
   description = "EC2 instance profile ARN"
   type        = string
+  ephemeral   = true
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  ephemeral   = true
 }
 
 variable "tags" {
   description = "Additional tags to apply to resources"
   type        = map(string)
   default     = {}
+  ephemeral   = true
 }
--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,15 +1,18 @@
 variable "name_prefix" {
   description = "Prefix for resource names"
   type        = string
+  ephemeral   = true
 }
 
 variable "environment" {
   description = "Environment name (dev, stg, prd)"
   type        = string
+  ephemeral   = true
 }
 
 variable "tags" {
   description = "Additional tags to apply to resources"
   type        = map(string)
   default     = {}
+  ephemeral   = true
 }
